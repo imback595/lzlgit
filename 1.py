3d0503329baa6cabf3c2499db07749f055cd58a2
lizhuolin <1213104204@qq.com>
2021-02-20 09:19:11 +0800

    add data change

--- 1.py
+++ 1.py
@@ -1,6 +1,11 @@
 # coding=utf-8
 # 导入Flask类
-from flask import Flask
+from flask import Flask,jsonify
+import pymssql
+import configparser
+cf=configparser.ConfigParser()
+cf.read('config.ini')
+
 
 # Flask 接收一个参数__name__，当前模块的文件名
 # Flask 在查找静态文件，或者模板时候默认以当前文件所在的目录去查找
@@ -13,7 +18,27 @@ app = Flask(__name__)
 def index():
     return "hello world"
 
+@app.route('/data')
+def print_data():
 
+	conn = pymssql.connect(host=cf.get("SqlServer-Database", "host"), user=cf.get("SqlServer-Database","user"), password=cf.get("SqlServer-Database","password"), database='LearnDb',autocommit=True)
+	cursor=conn.cursor()
+	sql="""select TOP 3 [Id],[url] FROM [LearnDb].[dbo].[pic]"""
+	try:
+		cursor.execute(sql)
+		data = cursor.fetchall()
+		cursor.close()
+		conn.close()
+		return jsonify({"urllist":data})
+	except Exception as e :
+		print(e)
+		cursor.close()
+		conn.close()
+		return e
+		
+	
+		
+		
 if __name__ == '__main__':
-    # Flask 应用程序实例的方法run启动web服务器
+	# Flask 应用程序实例的方法run启动web服务器
     app.run(host='0.0.0.0',debug=True)
--- dockerfile
+++ dockerfile
@@ -1,6 +1,6 @@
-FROM python:3.7
+FROM python:3.8-slim-buster
 COPY . /work
 WORKDIR /work
-RUN pip3 install -r requirements.txt
+RUN pip3 install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 ENTRYPOINT [ "python" ]
 CMD ["./1.py"]
